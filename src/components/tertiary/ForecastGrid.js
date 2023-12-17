6204a18055da73e8c88d5b92beab5369ffeb7bde
s <>
2023-12-17 16:06:08 +0100

    Add: Fading in when See more

--- src/components/tertiary/ForecastGrid.js
+++ src/components/tertiary/ForecastGrid.js
@@ -1,23 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useDataContext } from '../../context/DataContext';
 import ForecastDay from "./ForecastDay";
 import { convertIconSVG } from "../../utils/iconManipulation";
+import gsap from "gsap";
 
 const ForecastGrid = () => {
     const { data, loading, option } = useDataContext();
+    var tl = new gsap.timeline();
     const [seeMore, setSeeMore] = useState(false);
+    const [seeMoreText, setSeeMoreText] = useState(false);
     if (loading) return;
 
     const handleSeeMore = () => {
-        setSeeMore(!seeMore);
+        tl.add(function () { setSeeMoreText(!seeMore); })
+        tl.fromTo(".small-glass", { opacity: 1, duration: 0.5 }, { opacity: 0, duration: 0.5 })
+        tl.add(function () { setSeeMore(!seeMore); })
     };
 
     const forecastPath = data["forecast"]["forecastday"];
     const mode = option.mode;
 
     const forecastList = forecastPath.map((day, index) => {
-        if(seeMore && index < 7) return null;
-        if(!seeMore && index > 6) return null;        
+        if (seeMore && index < 7) return null;
+        if (!seeMore && index > 6) return null;
 
         return <ForecastDay
             key={day["date_epoch"]}
@@ -34,7 +39,7 @@ const ForecastGrid = () => {
     return (
         <>
             {forecastList}
-            <div className="see forecast-more flex-center" onClick={handleSeeMore}  style={{ textAlign: "center" }}><span>{seeMore ? "See less":"See more"}</span></div>
+            <div className="see forecast-more flex-center" onClick={handleSeeMore} style={{ textAlign: "center" }}><span>{seeMoreText ? "See less" : "See more"}</span></div>
         </>
     );
 };
